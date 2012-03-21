af5b9874634e0ffc30ea4880830159c5b9a71ed9
daisy1754 <d-part@hotmail.co.jp>
2012-03-21 21:25:31 +0900

    refactored: split files, remove unnecessary space, change indent

--- dict_history/english_log.js
+++ dict_history/english_log.js
@@ -1,39 +1,38 @@
 // script that logs the searched English word.
 
-console.log(location.href);
-
-var url = location.href;
-var rawWord = '';
-if (url.search('eow.alc') > 0) {
-	rawWord = url.split('/')[3];
-} else if (url.search('weblio.jp') > 0) {
-	rawWord = url.split('/')[4];
-} else if (url.search('dictionary.goo') > 0) {
-	rawWord = url.split('/')[5];
-} else if (url.search('ldoceonline') > 0) {
-	var query = window.location.search.substring(1);
-	var params = query.split('&');
-	for (var i=0; i<params.length; i++) {
-		var getParam = params[i].split('=');
-		if (getParam[0] == 'q') {
-			rawWord = getParam[1];
-			break;
-		}
-	}
-} else if (url.search('excite.co.jp') > 0) {
-	var query = window.location.search.substring(1);
-	var params = query.split('&');
-	for (var i=0; i<params.length; i++) {
-		var getParam = params[i].split('=');
-		if (getParam[0] == 'search') {
-			rawWord = getParam[1];
-			break;
-		}
-	}
-}
+var rawWord = getWordFromURL(location.href);
 
 // is English word?
-if (!rawWord.match(/[^a-zA-Z\.\-\+]+/)) {
+if (rawWord && !rawWord.match(/[^a-zA-Z\.\-\+]+/)) {
 	rawWord = rawWord.replace(/\+/g, ' ');
 	chrome.extension.sendRequest(rawWord);
+}
+
+function getWordFromURL(url) {
+	if (url.search('eow.alc') > 0) {
+		return url.split('/')[3];
+	} else if (url.search('weblio.jp') > 0) {
+		return url.split('/')[4];
+	} else if (url.search('dictionary.goo') > 0) {
+		return url.split('/')[5];
+	} else if (url.search('ldoceonline') > 0) {
+		var query = window.location.search.substring(1);
+		var params = query.split('&');
+		for (var i=0; i<params.length; i++) {
+			var getParam = params[i].split('=');
+			if (getParam[0] == 'q') {
+				return getParam[1];
+			}
+		}
+	} else if (url.search('excite.co.jp') > 0) {
+		var query = window.location.search.substring(1);
+		var params = query.split('&');
+		for (var i=0; i<params.length; i++) {
+			var getParam = params[i].split('=');
+			if (getParam[0] == 'search') {
+				return getParam[1];
+			}
+		}
+	}
+	return null;
 }
