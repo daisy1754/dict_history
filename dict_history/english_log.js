df85b81ede5ca0ee5238b26e9b47a07780e4630b
daisy1754 <d-part@hotmail.co.jp>
2012-05-13 17:29:12 +0900

    add shortcut feature

--- dict_history/english_log.js
+++ dict_history/english_log.js
@@ -1,12 +1,21 @@
 // script that logs the searched English word.
 
+const KEY_CODE_E = 69;
+const KEY_CODE_R = 82;
+const KEY_CODE_T = 84;
+const POPUP_MSEC = 1000;
+
+var popup = undefined;
 var rawWord = getWordFromURL(location.href);
 
 // is English word?
 if (rawWord && !rawWord.match(/[^a-zA-Z\.\-\+]+/)) {
 	rawWord = rawWord.replace(/\+/g, ' ');
-	chrome.extension.sendRequest(rawWord);
+	chrome.extension.sendRequest('add:' + rawWord);
 }
+
+document.addEventListener('keydown', keyDownListener);
+var commandStarted = false;
 
 function getWordFromURL(url) {
 	if (url.search('eow.alc.co.jp/search?') > 0) {
@@ -35,4 +44,66 @@ function getParameter(key) {
 		}
 	}
 	return null;
+}
+
+function keyDownListener(event) {
+	if (!commandStarted) {
+		if (event.keyCode == KEY_CODE_E && event.ctrlKey && event.shiftKey) {
+			console.log("command start");
+			commandStarted = true;
+			showPopupItem("Dict history loggerコマンド一覧");
+			event.preventDefault();
+		}
+	} else {
+		if (event.keyCode == KEY_CODE_R) {
+			var rawWord = getWordFromURL(location.href);
+			if (rawWord && !rawWord.match(/[^a-zA-Z\.\-\+]+/)) {
+				rawWord = rawWord.replace(/\+/g, ' ');
+				chrome.extension.sendRequest('remove:' + rawWord);
+				showPopupItem("'" + rawWord + "' がログから取り除かれました．");
+			} else {
+				dismissPopupItem();
+			}
+		} else if (event.keyCode == KEY_CODE_T){
+			// TODO: register as test.
+		} else {
+			dismissPopupItem();
+		}
+		commandStarted = false;
+		event.preventDefault();
+	}
+}
+
+function showPopupItem(message, durationInMillis) {
+	if (typeof popup == 'undefined') {
+		popup = document.createElement('div');
+		popup.width = '200px';
+		popup.height = '500px';
+		popup.style['background-color'] = '#cfcfdf';
+		popup.style['z-index'] = 10;
+		popup.style.border = '3px solid #bfbfcf';
+		popup.style.position = 'absolute';
+		popup.style.top = 0;
+		popup.style.right = 0;
+		popup.style.padding = '0.8em';
+		document.body.appendChild(popup);
+		
+		if (popup.timer) {
+			clearTimeout(popup.timer);
+		}
+		
+	} else {
+		popup.style.display = 'block';
+	}
+	
+	if (durationInMillis > 0) {
+		popup.timer = setTimeout(dismissPopupItem, durationInMillis);
+	}
+
+	popup.innerHTML = message;
+}
+
+function dismissPopupItem() {
+	popup.timer = undefined;
+	popup.style.display = 'none';
 }
